d683a08fd14049a4039ad7c23e7abf0c4e275768
abien.agarap <abien.agarap@senti.com.ph>
2019-09-23 20:54:34 +0800

    docs : update author

--- notebooks/models/lenet.py
+++ notebooks/models/lenet.py
@@ -18,6 +18,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+__author__ = 'Abien Fred Agarap'
 __version__ = '1.0.0'
 
 import tensorflow as tf
