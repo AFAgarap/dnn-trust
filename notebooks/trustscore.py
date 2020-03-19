e265bf571214ae5e8254601e83eb9091f8fe5a33
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-03-19 21:40:00 +0800

    docs: add license header

--- notebooks/trustscore.py
+++ notebooks/trustscore.py
@@ -1,3 +1,18 @@
+# Analyzing Deep Learning Models based on Trust Score
+# Copyright (C) 2019  Abien Fred Agarap
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """Modified trust score module from Google and Seldon Alibi"""
 from __future__ import absolute_import
 from __future__ import division
